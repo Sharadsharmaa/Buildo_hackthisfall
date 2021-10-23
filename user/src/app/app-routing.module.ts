bed235523da2e0e7d35791529006b835d82162fd
sukriti <sukriti.singh899@gmail.com>
2021-10-24 03:17:45 +0530

    first commit

--- user/src/app/app-routing.module.ts
+++ user/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 const routes: Routes = [
-  { path: '', data: { header: true, footer: false }, loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: '', data: { header: true, footer: true }, loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'login', data: { header: false, footer: false }, loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'register', data: { header: true, footer: false }, loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
-  { path: 'property', data: { header: true, footer: false }, loadChildren: () => import('./property/property.module').then(m => m.PropertyModule) }
+  { path: 'register', data: { header: true, footer: true }, loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+  { path: 'property', data: { header: true, footer: true }, loadChildren: () => import('./property/property.module').then(m => m.PropertyModule) }
 ];
 
 @NgModule({
